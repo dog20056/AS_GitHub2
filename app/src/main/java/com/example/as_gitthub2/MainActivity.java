ae2d89a62f67993129abb8a0f16f89d1a2ef19ee
Admin <noname20050624@gmail.com>
2024-06-20 21:44:01 +0900

    asd

---
+++ app/src/main/java/com/example/as_gitthub2/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.as_gitthub2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.txt1);
+    }
+
+
+    public void onClick(View view) {
+
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+        final Call<User> call =
+                gitHubService.getUser("alexanderklimov");
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                // response.isSuccessfull() is true if the response code is 2xx
+                if (response.isSuccessful()) {
+                    User user = response.body();
+
+                    // Получаем json из github-сервера и конвертируем его в удобный вид
+                    mTextView.setText("Аккаунт Github: " + user.getName() +
+                            "\nСайт: " + user.getBlog() +
+                            "\nКомпания: " + user.getCompany());
+                } else {
+                    int statusCode = response.code();
+
+                    // handle request errors yourself
+                    ResponseBody errorBody = response.errorBody();
+                    try {
+                        mTextView.setText(errorBody.string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable throwable) {
+                mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
+            }
+        });
+    }
+}
